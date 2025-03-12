55e0d83157008721326ac7406918996c0e3d1b01
Alexander <127344545+alexanderthebadatcoding@users.noreply.github.com>
2025-03-12 10:47:15 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     quality: 90, // Adjust image quality (default is 75)
     formats: ["image/webp", "image/png"], // Control allowed formats
     minimumCacheTTL: 60 * 60 * 24, // Cache images for 1 day
-    unoptimized: false, // Set to true if you want to disable optimization globally
+    unoptimized: true, // Set to true if you want to disable optimization globally
   },
 };
 
