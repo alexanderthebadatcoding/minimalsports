fd2cc49668d29d4ca3c8c987c872234cb7732e90
Alexander <127344545+alexanderthebadatcoding@users.noreply.github.com>
2025-03-26 10:57:32 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     { name: "English Premier League", path: "/epl" },
     { name: "FIFA Friendlies", path: "/fifa" },
     { name: "FIFA Womens Friendlies", path: "/fifaw" },
-		{ name: "Concacaf Nations League", path: "/concacaf" },
+		// { name: "Concacaf Nations League", path: "/concacaf" },
     { name: "Concacaf Champions Cup", path: "/concacafCup" },
     // { name: "She Believes Cup", path: "/sheBelieves" },
     // Add more pages as needed
